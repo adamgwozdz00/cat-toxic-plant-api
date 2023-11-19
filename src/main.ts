ce721f68c0a332db51519ef89089a0aaa96cdf79
adamgwozdz00 <adamgwozdz00@gmail.com>
2023-11-19 13:11:27 +0100

    format + add banner

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { banner } from './banner';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle("cat-app")
-    .setVersion("1.0")
+    .setTitle('cat-app')
+    .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
+  console.log(banner);
 }
 bootstrap();
